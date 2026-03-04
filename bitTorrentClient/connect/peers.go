eeaeaf487d82b7294ce841a5e3888f5a5c9b0ea0
Wesley Lawson <lawsonwe@bc.edu>
2026-03-03 20:12:54 -0500

    Updating peers.go

--- bitTorrentClient/connect/peers.go
+++ bitTorrentClient/connect/peers.go
@@ -3,55 +3,31 @@ package connect
 import (
 	"encoding/binary"
 	"errors"
-	"net"
-	"net/url"
-	"strconv"
+	"net/netip"
 )
 
-// Peer encodes connection information for a peer
+const peerLen = 6
+
 type Peer struct {
-	IP   net.IP
+	IP   netip.Addr
 	Port uint16
 }
 
-// Unmarshal parses peer IP addresses and ports from a buffer
-func Unmarshal(peersBin []byte) ([]Peer, error) {
+func parsePeers(rawPeers []byte) ([]Peer, error) {
 
-	const peerSize = 6 // 4 for IP, 2 for port
-	numPeers := len(peersBin) / peerSize
-	if len(peersBin)%peerSize != 0 {
-		err := errors.New("Received malformed peers")
-		return nil, err
+	peerSliceLen := len(rawPeers)
+	if peerSliceLen%peerLen != 0 {
+		return nil, errors.New("Malformed peers package: raw binary data is not a multiple of 6.")
 	}
 
-	peers := make([]Peer, numPeers)
-	for i := range numPeers {
-		offset := i * peerSize
-		peers[i].IP = net.IP(peersBin[offset : offset+4])
-		peers[i].Port = binary.BigEndian.Uint16(peersBin[offset+4 : offset+6])
+	var ret []Peer
+	for i := 0; i < peerSliceLen; i += peerLen {
+		var p Peer
+		p.IP = netip.AddrFrom4([4]byte(rawPeers[i : i+4]))
+		p.Port = binary.BigEndian.Uint16(rawPeers[i+4 : i+6])
+
+		ret = append(ret, p)
 	}
 
-	return peers, nil
-}
-
-func (t *TorrentFile) buildTrackerURL(peerID [20]byte, port uint16) (string, error) {
-
-	base, err := url.Parse(t.Announce)
-	if err != nil {
-		return "", err
-	}
-
-	params := url.Values{
-		"peer_id":    []string{string(peerID[:])},
-		"port":       []string{strconv.Itoa(int(port))},
-		"uploaded":   []string{"0"},
-		"downloaded": []string{"0"},
-		"compact":    []string{"1"},
-		"left":       []string{strconv.Itoa(t.Length)},
-	}
-	params.Set("info_hash", string(t.InfoHash[:]))
-	params.Set("event", "started")
-
-	base.RawQuery = params.Encode()
-	return base.String(), nil
+	return ret, nil
 }
